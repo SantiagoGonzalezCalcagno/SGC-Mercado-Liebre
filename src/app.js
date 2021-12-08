a2dbb071afbef7fb9ef9161699515130c6943a5a
SantiagoGonzalezCalcagno <s.gonzalezcalcagno@gmail.com>
2021-12-08 17:40:36 -0300

    register.html incompleta

--- src/app.js
+++ src/app.js
@@ -8,5 +8,5 @@ app.set('port', 3000);
 app.listen(3000, () => console.log("listening on port http://localhost:" + app.set('port')));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
-app.get('/log-in/', (req, res) => res.sendFile(path.resolve(__dirname, './views/log-in.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
 app.get('/sign-in/', (req, res) => res.sendFile(path.resolve(__dirname, './views/sign-in.html')));
