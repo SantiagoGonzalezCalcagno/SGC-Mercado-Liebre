039a3bfcd7ccb60c954a96e39f1560d24dd460e1
SantiagoGonzalezCalcagno <s.gonzalezcalcagno@gmail.com>
2021-12-09 20:07:00 -0300

    Anoche comi empanadas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.set('port', process.env.PORT || 3000);
-app.listen(3000, () => console.log("listening on port http://localhost:" + app.set('port')));
+app.listen(app.get('port'), () => console.log("listening on port http://localhost:" + app.set('port')));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
